6e673dae91775a51846ce055c17e14fa9572c545
niroberto <robertwilly668@gmail.com>
2023-02-21 09:22:30 +0200

    fix  deployment

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// export default axios.create({
-//   baseURL: "https://blogapi-production-87cd.up.railway.app/api/v1/",
-// });
-
 export default axios.create({
-  baseURL: "http://127.0.0.1:5000/api/v1/",
+  baseURL: "https://blogapi-production-87cd.up.railway.app/api/v1/",
 });
+
+// export default axios.create({
+//   baseURL: "http://127.0.0.1:5000/api/v1/",
+// });
 
 
